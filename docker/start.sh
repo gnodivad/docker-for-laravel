329faac78e52e3a23784529d0d16dca65a15d674
David <dodwei.dev@gmail.com>
2018-10-24 19:10:28 +0800

    Add custom start script

---
+++ docker/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+env=${APP_ENV:-production}
+
+echo "The environment is $env"
+
+if [ "$env" != "local" ]; then
+    echo "Removing XDebug"
+    rm -rf /usr/local/etc/php/conf.d/{docker-php-ext-xdebug.ini,xdebug.ini}
+fi
+
+exec apache2-foreground

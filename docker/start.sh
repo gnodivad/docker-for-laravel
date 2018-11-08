e8252919e8bff24d29a15478ff6560352d1240f9
David <dodwei.dev@gmail.com>
2018-11-08 15:30:20 +0800

    Use supervisor for queue and scheduler

--- docker/start.sh
+++ docker/start.sh
@@ -20,16 +20,20 @@ fi
 echo "The role is $role"
 
 if [ "$role" = "app" ]; then
+    ln -sf /etc/supervisor/conf.d-available/app.conf /etc/supervisor/conf.d/app.conf
     exec supervisord -c /etc/supervisor/supervisord.conf
 elif [ "$role" = "scheduler" ]; then
+    # ln -sf /etc/supervisor/conf.d-available/scheduler.conf /etc/supervisor/conf.d/scheduler.conf
     while [ true ]
     do
         php /var/www/html/artisan schedule:run --verbose --no-interaction &
         sleep 60
     done
 elif [ "$role" = "queue" ]; then
-    echo "Running the queue"
-    exec php /var/www/html/artisan queue:work --verbose --tries=3 --timeout=90
+    ln -sf /etc/supervisor/conf.d-available/queue.conf /etc/supervisor/conf.d/queue.conf
+    exec supervisord -c /etc/supervisor/supervisord.conf
+    # echo "Running the queue"
+    # exec php /var/www/html/artisan queue:work --verbose --tries=3 --timeout=90
 else
     echo "Could not match the container role \"$role\""
     exit 1
